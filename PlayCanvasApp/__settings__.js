6fd4b33d362a69619058725c6782551eb7338df8
Ty Radman <timdesign1996@gmail.com>
2025-01-20 15:58:53 -0800

    Initial

---
+++ PlayCanvasApp/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1956787.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 168641489, 169941217, 170170077, 175720995, 176100442, 177071245, 177071269, 182270433 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/168564884/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/168564885/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/168564883/1/ammo.js', 'preload' : true},
+];
