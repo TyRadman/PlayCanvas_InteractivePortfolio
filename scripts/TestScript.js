a76fc85ded4c96ab843558392ca221191a490911
Ty Radman <timdesign1996@gmail.com>
2025-01-25 17:40:43 -0800

    Automated sync from PlayCanvas

---
+++ scripts/TestScript.js
@@ -0,0 +1,18 @@
+var TestScript = pc.createScript('testScript');
+
+// initialize code called once per entity
+TestScript.prototype.initialize = function() {
+
+};
+
+// update code called every frame
+TestScript.prototype.update = function(dt) {
+
+};
+
+// uncomment the swap method to enable hot-reloading for this script
+// update the method body to copy state from the old instance
+// TestScript.prototype.swap = function(old) { };
+
+// learn more about scripting here:
+// https://developer.playcanvas.com/user-manual/scripting/
